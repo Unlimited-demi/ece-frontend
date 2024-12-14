8e3ba4b44913ee0a6d7d83b57450cc24bad914b7
Daniel Owen <unlimiteddemi@gmail.com>
2024-12-14 17:41:17 +0100

    fixing login page not found

--- src/router/router.js
+++ src/router/router.js
@@ -1,26 +1,23 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-
+import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "../stores/store";
 
 const routes = [
-  { path: "/login", name: "Login", component: () => import("../views/Login.vue") },
-  { path: "/", name: "Home", component: () => import("../views/Home.vue") },
+  { path: "/login", name: "Login", component: () => import("../views/Login.vue"), meta: { requiresAuth: false } },
+  { path: "/", name: "Home", component: () => import("../views/Home.vue"), meta: { requiresAuth: true } },
 ];
 
-
 const router = createRouter({
-  history: createWebHashHistory(), // Use this
+  history: createWebHistory(),
   routes,
 });
-
 
 // Global route guard
 router.beforeEach((to, from, next) => {
   const store = useAuthStore();
   const token = store.token || localStorage.getItem("authToken");
 
-  if (to.name !== "Login" && !token) {
-    return next({ name: "Login" }); // Redirect to login
+  if (to.meta.requiresAuth && !token) {
+    return next({ name: "Login", query: { redirect: to.fullPath } }); // Redirect to login with redirect query
   }
 
   if (token && !store.token) {
